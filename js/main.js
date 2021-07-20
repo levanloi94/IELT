1d5125a98b4c7fe0d2cdb22f7c2f7a047635754e
Van Loi <vanloi1501@gmail.com>
2021-07-20 09:45:11 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 
 // ONSROLL NAV
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 const backToTopBtn = document.querySelector('.back-to-top');
 function scrollFunction() {
-        if (document.body.onscrollTop >200 || document.documentElement.scrollTop > 200) {
-            document.querySelector('.header-top').style.display ='none';
-            backToTopBtn.style.opacity = '1';
-            backToTopBtn.style.visibility = 'visible';
-        }else{  
-            document.querySelector('.header-top').style.display ='';
-            backToTopBtn.style.opacity = '';
-            backToTopBtn.style.visibility = '';
-        }      
+    if (document.body.onscrollTop > 150 || document.documentElement.scrollTop > 150) {
+        document.querySelector('.header-top').style.display = 'none';
+        backToTopBtn.style.opacity = '1';
+        backToTopBtn.style.visibility = 'visible';
+    } else {
+        document.querySelector('.header-top').style.display = '';
+        backToTopBtn.style.opacity = '';
+        backToTopBtn.style.visibility = '';
+    }
 }
 
 //BACK TO TOP FUNC
 
-backToTopBtn.addEventListener('click', ()=>{
+backToTopBtn.addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 })
 
@@ -24,37 +24,49 @@ backToTopBtn.addEventListener('click', ()=>{
 const body = document.querySelector('body');
 const openMenuMb = document.querySelector('.collapse-menu');
 const menuMb = document.querySelector('.header .header-menu');
-const menuMbActived =document.querySelector('.header-menu.is-active');
+const menuMbActived = document.querySelector('.header-menu.is-active');
 const openMenuMbActived = document.querySelector('.collapse-menu.is-active');
 
-openMenuMb.addEventListener('click', ()=>{
+openMenuMb.addEventListener('click', () => {
     openMenuMb.classList.toggle('is-active');
     menuMb.classList.toggle('is-active');
     body.classList.toggle('is-active');
 })
 
-document.addEventListener('click', (e) =>{
-    if(!menuMb.contains(e.target) && 
-       !openMenuMb.contains(e.target)){
+document.addEventListener('click', (e) => {
+    if (!menuMb.contains(e.target) &&
+        !openMenuMb.contains(e.target)) {
         openMenuMb.classList.remove('is-active');
         menuMb.classList.remove('is-active');
         body.classList.remove('is-active');
-       }
+    }
 })
 
+//submenu mobile
+const menuItem = document.querySelectorAll('.header-menu-item');
+
+for (let item of menuItem) {
+    item.addEventListener(('click'), () => {
+        item.classList.toggle('is-active');
+    })
+}
 
 const swiper = new Swiper('.banner .swiper-container', {
     slidesPerView: 1,
     autoplay: {
         delay: 5000,
     },
-  
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
 });
-const swiper2 = new Swiper('.refer .swiper-container',{
+const swiper2 = new Swiper('.refer .swiper-container', {
     slidesPerView: 2,
     // centeredSlides: true,
     // slidesOffsetBefore:10,
     spaceBetween: 15,
+    loop: true,
     navigation: {
         prevEl: '.slidePrev-btn',
         nextEl: '.slideNext-btn'
@@ -62,19 +74,17 @@ const swiper2 = new Swiper('.refer .swiper-container',{
     breakpoints: {
         // when window width is >= 767px
         767: {
-          slidesPerView: 4,
-          spaceBetween: 30
+            slidesPerView: 4,
+            spaceBetween: 30
         }
     },
     autoplay: {
         delay: 3000,
     }
-    
-    
 });
 // swiper2.slideTo(5);
 
-const swiper3 = new Swiper('.tips .swiper-container',{
+const swiper3 = new Swiper('.tips .swiper-container', {
     spaceBetween: 20,
     navigation: {
         prevEl: '.slidePrev-btn',
@@ -85,34 +95,39 @@ const swiper3 = new Swiper('.tips .swiper-container',{
     }
 })
 
-const swiperTeacher = new Swiper('.teachers .swiper-container',{
+const swiperTeacher = new Swiper('.teachers .swiper-container', {
     slidesPerView: 2,
     spaceBetween: 10,
+    loop: true,
     autoplay: {
         delay: 3000,
     },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
     breakpoints: {
         // when window width is >= 767px
         767: {
-          slidesPerView: 4,
-          spaceBetween: 30
+            slidesPerView: 4,
+            spaceBetween: 30
         }
     },
 })
-const swiperWhat = new Swiper('.what .swiper-container',{
+const swiperWhat = new Swiper('.what .swiper-container', {
     slidesPerView: 1,
     autoplay: {
         delay: 2000,
     }
 })
-const swpiperCourseRelate  = new Swiper('.courserelate .swiper-container',{
+const swpiperCourseRelate = new Swiper('.courserelate .swiper-container', {
     slidesPerView: 1,
     spaceBetween: 10,
 
     pagination: {
         el: '.swiper-pagination',
-      },
-      
+    },
+
     autoplay: {
         delay: 2000,
     },
@@ -120,25 +135,25 @@ const swpiperCourseRelate  = new Swiper('.courserelate .swiper-container',{
         650: {
             slidesPerView: 2,
             spaceBetween: 10
-          },
+        },
         // when window width is >= 930px
         930: {
-          slidesPerView: 3,
-          spaceBetween: 15
+            slidesPerView: 3,
+            spaceBetween: 15
         }
     },
 })
-    
+
 
 
 
 //active pagination
 const paginationItem = document.querySelectorAll('.pagination-item');
-const pagination =  document.querySelector('.pagination');
+const pagination = document.querySelector('.pagination');
 
-if(pagination){
-    for(let i =0; i< paginationItem.length; i++){
-        paginationItem[i].addEventListener('click', ()=>{
+if (pagination) {
+    for (let i = 0; i < paginationItem.length; i++) {
+        paginationItem[i].addEventListener('click', () => {
             console.log('click')
             let currItem = document.querySelector('.pagination-item.is-active');
             currItem.classList.remove('is-active');
@@ -149,9 +164,9 @@ if(pagination){
 //Toggle like
 const likeBtn = document.querySelectorAll('.like');
 
-if(likeBtn){
-    for(let i = 0; i< likeBtn.length; i++){
-        likeBtn[i].addEventListener('click',() => {
+if (likeBtn) {
+    for (let i = 0; i < likeBtn.length; i++) {
+        likeBtn[i].addEventListener('click', () => {
             likeBtn[i].classList.toggle('is-active');
         })
     }
